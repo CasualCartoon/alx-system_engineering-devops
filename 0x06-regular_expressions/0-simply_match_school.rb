a11c5f7c541eb0eb5059175cf5a95097f5b38c76
CasualCartoon <muiruriian4@gmail.com>
2024-03-06 04:52:13 -0800

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,16 +1,2 @@
 #!/usr/bin/env ruby
-
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <string>"
-  exit 1
-end
-
-regexp = /School/
-
-matches = ARGV[0].scan(regexp)
-
-if matches.empty?
-  puts "No occurrences of 'School' found in the input."
-else
-  puts "Occurrences of 'School' found: #{matches.join(', ')}"
-end
+puts ARGV[0].scan(/School/).join
